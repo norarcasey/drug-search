72482d20847153b9f6f8dd5df775429c928343df
Nora Casey <1891757+norarcasey@users.noreply.github.com>
2024-03-01 10:39:25 -0800

    chore: working on nextjs deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/drug-search",
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
+};
 
 export default nextConfig;
