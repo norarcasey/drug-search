cece782095169a5927ee9bec213e71a1e8903930
Nora Casey <1891757+norarcasey@users.noreply.github.com>
2024-03-01 13:39:08 -0800

    feat: add basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,51 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { useState } from "react";
+
+import { data } from "./data";
+// import styles from "./page.module.css";
+
+function DrugResult({ result }: { result: any }): React.ReactElement {
+  return (
+    <div className="flex flex-col border p-5 capitalize max-w-100 max-h-100">
+      <p className="mb-3">
+        <strong>{result.generic_name}</strong>
+      </p>
+      <p>
+        <strong>Brand name:</strong> {result.brand_name}
+      </p>
+      <div>
+        <strong>Active ingredients</strong>
+        {result.active_ingredients.map((ingredient: any) => (
+          <p>{ingredient.name}</p>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default function Home() {
+  const [search, setSearch] = useState("");
+  const { results } = data;
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="flex flex-col p-20 gap-5">
+      <h1 className="text-3xl pb-10">Drug Search</h1>
+      <div className="text-sm text-gray-500">{data.meta.disclaimer}</div>
+      <input
+        type="text"
+        className="text-lg border mb-10 p-2 w-1/2"
+        placeholder="Search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <div className="flex flex-wrap flex-auto gap-2">
+        {results.map((result, index) => (
+          <DrugResult key={`drug-result-${index}`} result={result} />
+        ))}
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      {/* TODO: Add a search box */}
+    </div>
   );
 }
